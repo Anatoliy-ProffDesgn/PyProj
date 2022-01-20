c21d02c36bd864ca3ad7891329e8e3695dded437
natol_ba3j6nf <natolij19831@gmail.com>
2022-01-20 18:10:22 +0200

    Initial commit

---
+++ MyTrtle1.py
@@ -0,0 +1,4 @@
+import turtle as tr
+tr.setup(800,800)   #Задать размеры экрана
+tr.fd(80)
+tr.mainloop()
---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for i in range(1 , 5):
+    print(i)
+'''
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    A = 'PyCharm'
+    print_hi(A)
+    A = 'MeCharm'
+    print_hi(A)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/'''
